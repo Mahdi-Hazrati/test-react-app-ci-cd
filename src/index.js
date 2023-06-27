2327cee893c69875bfaaf4e910b02d9cfa0ee1da
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-06-28 00:17:51 +0430

    change content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import ReactDOM from 'react-dom/client';
-ReactDOM.createRoot(document.getElementById('root')).render(<div>React workflow - By Mahdi Hazrati</div>);
+ReactDOM.createRoot(document.getElementById('root')).render(<div>Re Factored - React workflow</div>);
