bcf738f733cd2e2ad78f4c70946626013ffc7486
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-06-27 23:57:09 +0430

    ddd

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import ReactDOM from 'react-dom/client';
-ReactDOM.createRoot(document.getElementById('root')).render(<div>React workflow - Github - Mahdi Hazrati - Done!</div>);
+ReactDOM.createRoot(document.getElementById('root')).render(<div>React workflow - By Mahdi Hazrati</div>);
