5da1c8a95d7e7c71fb5acbe88de11c61e9208cc4
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-06-27 23:09:20 +0430

    fina

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import ReactDOM from 'react-dom/client';
-ReactDOM.createRoot(document.getElementById('root')).render(<div>React App CI CD - Github - Mahdi Hazrati</div>);
+ReactDOM.createRoot(document.getElementById('root')).render(<div>Test React App CI CD - Github - Mahdi Hazrati</div>);
