b58d85e6c77e8ebd1cbabd8e167e81696f8314d7
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-06-27 23:51:56 +0430

    latest test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import ReactDOM from 'react-dom/client';
-ReactDOM.createRoot(document.getElementById('root')).render(<div>TReact App CI CD - Github - Mahdi Hazrati - Done! - Now</div>);
+ReactDOM.createRoot(document.getElementById('root')).render(<div>React workflow - Github - Mahdi Hazrati - Done!</div>);
