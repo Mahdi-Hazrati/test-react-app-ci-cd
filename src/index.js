1fcca9e9d1278dbe05275f7cbb6ebadd501ef2e6
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-06-27 23:45:30 +0430

    Please God Help

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import ReactDOM from 'react-dom/client';
-ReactDOM.createRoot(document.getElementById('root')).render(<div>React App CI CD - Github - Mahdi Hazrati - Done!</div>);
+ReactDOM.createRoot(document.getElementById('root')).render(<div>TReact App CI CD - Github - Mahdi Hazrati - Done! - Now</div>);
